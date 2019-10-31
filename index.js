c5d4d20827b1440ef38e4f5f3c473b04d24640a4
Robert <rc3331@gmail.com>
2019-10-31 12:50:10 -0500

    committing changes to env

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ server.get('/', (req, res) => {
   res.status(200).json({ hello: 'Web 23' });
 });
 
-server.listen(4000, () => {
-  console.log('\n Server running on port 4000 \n');
+const port = process.env.PORT || 4000
+
+server.listen(port, () => {
+  console.log(`\n Server running on port ${port} \n`);
 });
